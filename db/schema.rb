c009e31a28586ee973fa63b9724aa6c589814e9d
Bre Baker <bakerbr@IT-Q94QY2RQHT.local>
2024-10-14 12:58:36 -0500

    more configuration stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_205513) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_205426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,9 +62,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_205513) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "posts"
